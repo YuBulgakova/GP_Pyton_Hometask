50f59cde80714a9673db6831262ff2d2bc421ea5
YuBulgakova <petkevichj@gmail.com>
2022-04-08 12:02:56 +0300

    task 1

--- Lesson3.py
+++ Lesson3.py
@@ -1,4 +1,21 @@
-print('Hi there!')
-
 #1
+def my_func1 ():
+    try:
+        num1 = int(input('Enter number 1:'))
+        num2 = int(input('Enter number 1:'))
+    except ValueError:
+        print('Numbers only!')
+        return
+    try:
+        result = num1/num2
+        return result
+    # except ValueError:
+    #     print('Numbers only!')
+    except ZeroDivisionError:
+        print('Деление на ноль (Zero Division)!')
+    # except ArithmeticError:
+    #     print('Arithmetic Error')
+
+print(my_func1())
+
 #2
